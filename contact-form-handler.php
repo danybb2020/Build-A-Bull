55f907722d7d9fc0511fd9e5cab23783bd105b38
danybb2020 <daniel.balderas@gmail.com>
2021-02-25 13:53:36 -0800

    Bugs fixed

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,18 +1,18 @@
 <?php
 $name= $_POST['name'];
-$visitor_email= $POST['email'];
-$message= $POST['message'];
+$visitor_email= $_POST['email'];
+$message= $_POST['message'];
 
 $email_from= 'buildabullfitness@gmail.com';
-$email_subject= 'New Form Submission';
+$email_subject= "New Form Submission";
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
-                    "User Message: $message.\n".
+                    "User Message: $message.\n";
 
  $to= "daniel.balderas@gmail.com";
  $headers = "From: $email_from \r\n";
  $headers .= "Reply-to: $visitor_email \r\n";
 
- mail ($to, $email_subject, $email_body , $headers);
+ mail ($to,$email_subject,$email_body,$headers);
  header("Location: index.html");
 ?>
